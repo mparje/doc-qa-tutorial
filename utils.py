a3b7a36ddc87ce422948e8a0142a829f66e9b67d
mparje <126131806+mparje@users.noreply.github.com>
2023-05-20 20:39:03 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,13 @@ from langchain.llms import OpenAI
 from langchain.vectorstores.faiss import FAISS
 from pypdf import PdfReader
 import streamlit as st
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 
